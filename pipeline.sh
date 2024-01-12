a7dd97a5456c3fb7f3c8c55ccd4fb9ffc588eda5
Chunfu-Shawn <xiaochunfu@126.com>
2024-01-13 00:39:59 +0800

    update

--- pipeline.sh
+++ pipeline.sh
@@ -5,12 +5,18 @@
 #Argument 5: FASTA with ORF proteins
 
 #Requirements: Python3, biopython, BLAST, PHAST (maf_parse), PRANK
+echo "Calculating multiple alignments. For huge multiple alignments, each alignment can take several minutes and we recommend to parallelize the searches for multiple ORFs."
+python3 1_extract_multiple_alignments.py \
+    -b $1 -m $2 -o $3 -f yes
 
-echo "Calculating multiple alignments. For huge multiple alignments (e.g., 120 mammals), each alignment can take several minutes and we recommend to parallelize the searches for multiple ORFs."
-python3 1_extract_multiple_alignments.py -b $1 -m $2 -o $3
+# Calculating ancestral sequences and estimnate intact ORF ancestrally
+echo "Calculating ancestral sequences and estimnate intact ORF ancestrally"
+bash 2.1_ancestral_sequences.sh $3 $4 $1.ancestors $5
 
-echo "Calculating ancestral sequences"
-bash 2_ancestral_sequences.sh $3 $4 $1.ancestors
-
-echo "Running BLAST across orthologous regions to find conserved proteins"
+# Running BLAST across orthologous regions to get conservation score
+echo "Running BLAST across orthologous regions to get conservation score"
 python3 3_sequence_conservation_mammals.py -i $3 -p $5
+
+# Running BLAST across orthologous regions to find specifc proteins
+echo "Running BLAST across orthologous regions to find specifc proteins"
+python3 4_sequence_specific.py --prot_dir $3 --prot_tar $5
